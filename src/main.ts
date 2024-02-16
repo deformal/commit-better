f4cf28769ddf7678e2476d75fb92a7ec49bff1ec
deformal <drake9211@gmail.com>
2024-02-16 05:36:05 +0530

    testing the cli tool test 2

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,24 @@
 
 import { exec } from "child_process";
 import { promisify } from "util";
+import { createInterface } from "readline";
 const execCommand = promisify(exec);
 
 async function gitStatus() {
+  try {
+    const gitStatus = "git status";
+    const { stdout, stderr } = await execCommand(gitStatus);
+    console.log(stdout);
+    if (stderr) {
+      throw new Error(stderr);
+    }
+  } catch (err) {
+    const error = err as Error;
+    console.error(error.message);
+  }
+}
+
+async function gitAddAll() {
   try {
     const gitStatus = "git add .";
     const { stdout, stderr } = await execCommand(gitStatus);
@@ -18,4 +33,28 @@ async function gitStatus() {
   }
 }
 
-gitStatus().catch((err) => console.error(err));
+async function gitCommit() {
+  try {
+    const question = "Please Enter a commit message: ";
+    const rl = createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    rl.question(question, async (message: string) => {
+      await gitAddAll();
+      const command = `git commit -m '${message}'`;
+      const { stdout, stderr } = await execCommand(command);
+      if (stderr) {
+        throw new Error(stderr);
+      }
+      rl.close();
+    });
+  } catch (err) {
+    const error = err as Error;
+    console.error(error.message);
+    return error.message;
+  }
+}
+
+gitCommit().catch((err) => console.error(err));
