cbbf8352e97873eba6ab9b67556400bd895e744e
deformal <drake9211@gmail.com>
2024-02-16 20:20:24 +0530

    Commit Summary
    ____
    
    Features
    
    ____
    
    Issues Fixed
    
    ____
    
    Improvements

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { CreateMarkdownFile } from "./commitMdGen";
 import { CommitQuestions } from "./types";
 import { promisify } from "util";
 
-function gitCommit() {
+async function gitCommit() {
   const command = `git commit -F $PWD/.git/commit_summary.md`;
   const questions: CommitQuestions = {
     summay: "",
@@ -14,59 +14,28 @@ function gitCommit() {
     issuesLinks: [],
     improvementsLinks: [],
   };
-  askSummary(questions);
-
-  // const message = await asyncQuestions("Please Enter a commit message: ");
-  // questions.summay = message;
-  // rl.close();
-
-  // rl.question(
-  //   "If there are any feature done in this commit paste the link here : ",
-  //   (message: string) => {
-  //     questions.featureLinks = [message];
-  //     rl.close();
-  //   }
-  // );
-
-  // rl.question(
-  //   "If there are any Issue Fixes done in this commit paste the link here : ",
-  //   (message: string) => {
-  //     questions.issuesLinks = [message];
-  //     rl.close();
-  //   }
-  // );
-
-  // rl.question(
-  //   "If there are any Improvements done in this commit paste the link here : ",
-  //   (message: string) => {
-  //     questions.improvementsLinks = [message];
-  //     rl.close();
-  //   }
-  // );
+  await askSummary(questions);
 
   CreateMarkdownFile(questions);
 
-  if (questions.summay.length > 0) {
-    exec(command, (err, stderr, stdout) => {
-      // if (err) console.log(err);
-      if (stderr) console.error(stderr);
-      if (stdout) console.info(stdout);
-    });
-  }
+  exec(command, (err, stderr, stdout) => {
+    if (stderr) console.error(stderr);
+    if (stdout) console.info(stdout);
+  });
 }
 
-function askSummary(questions: CommitQuestions): string {
-  let message: string = "";
-  const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+function askSummary(questions: CommitQuestions): Promise<string> {
+  return new Promise((resolve) => {
+    const rl = createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  rl.question("Please Enter a commit message: ", (ans) => {
-    message = ans;
-    rl.close();
+    rl.question("Please Enter a commit message: ", (ans) => {
+      rl.close();
+      resolve(ans);
+    });
   });
-  return message;
 }
 
 gitCommit();
